bcdef97675d4c0c3397d069f9cc765566b6c8077
hamedahmed143022 <hamedahmed143022@gmail.com>
2025-06-25 17:59:12 +0000

    add user model

---
+++ lib/features/profile/logic/user_model.dart
@@ -0,0 +1,42 @@
+class UserModel {
+  final String uid;
+  final String fullName;
+  final String email;
+  final String image;
+  final int followers;
+  final int following;
+  final int likes;
+
+  UserModel({
+    required this.uid,
+    required this.email,
+    required this.fullName,
+    required this.image,
+    required this.followers,
+    required this.following,
+    required this.likes,
+  });
+
+  factory UserModel.fromMap(Map<String, dynamic> map, String uid) {
+    return UserModel(
+      uid: uid,
+      fullName: map['fullName'],
+      email: map['email'],
+      image: map['image'] ?? 'assets/images/guestImage.avif',
+      followers: map['followers'] ?? 0,
+      following: map['following'] ?? 0,
+      likes: map['likes'] ?? 0,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'fullName': fullName,
+      'email': email,
+      'image': image,
+      'followers': followers,
+      'following': following,
+      'likes': likes,
+    };
+  }
+}
